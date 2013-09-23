0d079800d0c1982ad280f5959e1cc7d25681e0c1
Mac <mac@abctech-thailand.com>
2013-09-23 18:37:45 +0700

    Change dependency xml2js to xml2json

--- routes/main.js
+++ routes/main.js
@@ -2,11 +2,13 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('main_index');
     });
-    
+
     app.post('/builder', function(req, res) {
         console.log("=============== XML Content ===============");
         console.log(req.body.xmlContent);
         console.log("===========================================");
-        res.render('main_builder');
+        res.render('main_builder', {
+            'data':'test'
+        });
     });
 }
