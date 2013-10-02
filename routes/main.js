b6dbb167ee11bfc857219b05a315a28ac1d741bd
Mac <mac@abctech-thailand.com>
2013-10-02 08:37:15 +0700

    Add form to open jira xml file

--- routes/main.js
+++ routes/main.js
@@ -1,6 +1,8 @@
 module.exports = function(app, jiraxml2json) {
     app.get('/', function(req, res) {
-        res.render('main_index');
+        res.render('main_index', {
+            'jiraXmlUrl':'https://apidev.atlassian.net/sr/jira.issueviews:searchrequest-xml/temp/SearchRequest.xml?jqlQuery=Sprint = ${sprintNumber} ORDER BY created asc&tempMax=1000'
+        });
     });
 
     app.post('/builder', function(req, res) {
